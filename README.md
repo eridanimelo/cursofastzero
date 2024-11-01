15abdb2a09c122a6378b749c63d73c8ec5b4c8f6
Eridani Melo <eridanimelo@Eridanis-MacBook-Pro.local>
2024-10-31 23:08:31 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### Habilitar o ambiente virtual para que o poetry enxergue as dependencias
+
+poetry shell
+
+### Iniciar a aplicação
+fastapi dev meuarquivo.py
+
+
+### Parar reconhecer o autocomplete do vscode
+$ poetry new eridanus
+$ cd eridanus/
+$ python -m venv .venv
+$ poetry shell
---
+++ fast_zero/app.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserPublic, UserSchema
+
+app = FastAPI()
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    ...
+
